6a2394cc95fe1ffb89855c77f5fa10f5b73944d7
YonghoBae <dyddyd134@chungbuk.ac.kr>
2024-08-13 00:06:00 +0900

    first commit

---
+++ routes/study.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/list', async(req,res)=>{
+    res.render('study/list');
+});
+
+router.get('/create', async(req,res)=>{
+    res.render('study/create');
+});
+
+router.get('/modify', async(req,res)=>{
+    res.render('study/modify');
+});
+
+module.exports = router;
